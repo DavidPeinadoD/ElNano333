d6e486405c1b120109da4043f86f94c53836af6b
DavidPeinadoD <davidpeinadodiaz@gmail.com>
2023-09-25 12:22:14 +0200

    la 33 se acerca

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Lanzador lanzador = new Lanzador();
+        lanzador.main(args);
+    }
+}
