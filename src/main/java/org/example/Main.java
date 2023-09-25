7d4cabf9910516873fd64251c113ad1a9ccaccc6
DavidPeinadoD <113924648+DavidPeinadoD@users.noreply.github.com>
2023-09-25 10:32:48 +0000

    nanosexo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Lanzador lanzador = new Lanzador();
-        lanzador.main(args);
+        Lanzador.main(args);
     }
 }
